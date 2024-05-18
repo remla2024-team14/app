f1e5bb0abd462a003aaa17ed07e45d4cd32edb09
Dani Rogmans <danirogmans@gmail.com>
2024-05-18 18:44:53 +0200

    basic metrics and adding some rules for AlertManager

--- app.py
+++ app.py
@@ -4,9 +4,17 @@ from dotenv import load_dotenv
 from flask import Flask, request, render_template
 from libversion import VersionUtil
 import requests
+from prometheus_client import Counter, generate_latest, CONTENT_TYPE_LATEST
+
 
 app = Flask(__name__)
 load_dotenv()
+
+# Prometheus metrics
+feedback_yes_counter = Counter('feedback_yes_total', 'Total number of "Yes" feedbacks')
+feedback_no_counter = Counter('feedback_no_total', 'Total number of "No" feedbacks')
+total_requests_counter = Counter('requests_total', 'Total number of requests')
+
 
 from flask import Flask, request, render_template, jsonify
 
@@ -14,6 +22,11 @@ from flask import Flask, request, render_template, jsonify
 def home():
     version_info = VersionUtil.VersionUtil.get_version()
     return render_template("index.html", version=version_info)
+
+@app.before_request
+def before_request():
+    total_requests_counter.inc()
+
 
 @app.route('/predict', methods=['POST'])
 def predict():
@@ -34,6 +47,20 @@ def predict():
         return jsonify({'prediction': prediction})
     except requests.exceptions.RequestException as e:
         return jsonify({'error': 'Failed to reach model service', 'exception': str(e)}), 500
+
+@app.route('/feedback', methods=['POST'])
+def feedback():
+    feedback_type = request.json.get('feedback')
+    if feedback_type == 'yes':
+        feedback_yes_counter.inc()
+    elif feedback_type == 'no':
+        feedback_no_counter.inc()
+    return jsonify({'status': 'success'})
+
+@app.route('/metrics')
+def metrics():
+    return generate_latest(), 200, {'Content-Type': CONTENT_TYPE_LATEST}
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8000, debug=True)
