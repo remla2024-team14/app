dcd9d5f6bb3cdbfa31c2289e699601025d981ab3
Dani Rogmans <danirogmans@gmail.com>
2024-05-18 13:17:50 +0200

    interactive predictions to ask for feedback

--- app.py
+++ app.py
@@ -8,19 +8,17 @@ import requests
 app = Flask(__name__)
 load_dotenv()
 
+from flask import Flask, request, render_template, jsonify
 
 @app.route('/')
 def home():
     version_info = VersionUtil.VersionUtil.get_version()
     return render_template("index.html", version=version_info)
 
-
 @app.route('/predict', methods=['POST'])
 def predict():
-    # Receive data from the front end
     text = request.form['input_text']
     model_choice = request.form['model_select']
-
     model_service_url = os.getenv("MODEL_SERVICE_URL")
 
     payload = {
@@ -29,16 +27,13 @@ def predict():
     }
 
     try:
-        response = requests.post(model_service_url, json=payload, timeout=10)  # timeout in seconds
+        response = requests.post(model_service_url, json=payload, timeout=10)
         response.raise_for_status()
+        prediction = response.text  # Assuming response.text is your prediction result
 
-        # should get the response and return prediction
-        return response.text
+        return jsonify({'prediction': prediction})
     except requests.exceptions.RequestException as e:
-        # Handle connection errors, timeouts, etc.
-        error = {'error': 'Failed to reach model service', 'exception': str(e)}
-        return error
-
+        return jsonify({'error': 'Failed to reach model service', 'exception': str(e)}), 500
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8000, debug=True)  # Run on a different port to avoid conflict
+    app.run(host='0.0.0.0', port=8000, debug=True)
